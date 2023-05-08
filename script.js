7aa1cec5929e9a505df44370eabf54ec712f9a8c
StealthLeprechaun <122291693+StealthLeprechaun@users.noreply.github.com>
2023-05-08 16:33:58 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Fun page functions */
+
+function showFunction() {
+  document.getElementById("panel").style.display = "block";
+}
+
+function showFunction2() {
+  document.getElementById("panel2").style.display = "block";
+}
+
+function showFunction3() {
+  document.getElementById("panel3").style.display = "block";
+}
